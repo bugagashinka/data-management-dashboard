b60dadc1b276bba9d1d13a03990611525ac229f2
Bugagashinka <voloshyn.andrew@gmail.com>
2020-07-29 16:43:53 +0300

    Add base project structure

---
+++ src/ui/App/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+      asdsadjsd
+    </div>
+  );
+}
+
+export default App;
