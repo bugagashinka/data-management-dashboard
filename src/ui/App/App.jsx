95d775004d070201a4eeff6b8f7149103b231b9f
Bugagashinka <voloshyn.andrew@gmail.com>
2020-07-30 23:08:04 +0300

    Fix. Remove twice selection by keyboard navigation for input slider

--- src/ui/App/App.jsx
+++ src/ui/App/App.jsx
@@ -78,7 +78,14 @@ function App() {
               <div className="thumb-size__inner">
                 <button className="button smaller-btn" type="button"></button>
                 <div className="thumb-size__slider" tabIndex="0">
-                  <input className="input thumb-size__slider-input" type="range" min="1" max="3" value="2" />
+                  <input
+                    className="input thumb-size__slider-input"
+                    tabIndex="-1"
+                    type="range"
+                    min="1"
+                    max="3"
+                    value="2"
+                  />
                 </div>
                 <button className="button bigger-btn" type="button"></button>
               </div>
