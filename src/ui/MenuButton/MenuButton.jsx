e0a7eb9af30ff5140cc444b3153659e72c900bc6
Bugagashinka <voloshyn.andrew@gmail.com>
2020-08-02 20:44:03 +0300

    Add MenuButton component

---
+++ src/ui/MenuButton/MenuButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MenuButton = (props) => {
+  return (
+    <button className="button app__menu-btn menu-btn">
+      <div className="menu-btn__row1"></div>
+      <div className="menu-btn__row2"></div>
+      <div className="menu-btn__row3"></div>
+    </button>
+  );
+};
+
+export default MenuButton;
